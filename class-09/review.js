cddbbb789602d00893d459c18a6311bdbcdb1b76
Roger Huba <rogerhuba@gmail.com>
2020-08-21 12:52:39 -0700

    add class 9 review

---
+++ class-09/review.js
@@ -0,0 +1,17 @@
+// Review of Java Script Loops
+
+// For loop - Known amount of time that we want the loop to run.
+var timeToRun = 10;
+
+//                    i++
+for(var i = timeToRun; i >= 0; i = i - 1){
+    // Do a thing
+}
+
+// While loop - A condtition to be met
+guessingGame = true;
+
+while(guessingGame === true){
+    // Do a thing
+    // Do something to change guessingGame
+}
